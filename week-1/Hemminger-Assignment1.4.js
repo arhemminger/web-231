fda6634405e4347d3badb962508ce081bbac975c
Andrew Hemminger <arhemminger@gmail.com>
2018-12-01 17:21:12 -0600

    assignment 1.4 commit

---
+++ week-1/Hemminger-Assignment1.4.js
@@ -0,0 +1,45 @@
+/* 
+============================================ ;
+Title: Assignment 1.4 ; 
+Author: Professor Krasso ; 
+Date: 1 December 2018 ; 
+Modified By: Andrew Hemminger ; 
+Description: This program demonstrates the ; 
+use of JavaScript types, values, and ; 
+and variables in an application. ;
+=========================================== 
+*/
+
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Andrew";
+var lastName = "Hemminger";
+var age = "31";
+var street = "700 S. 72nd St";
+var city = "Omaha";
+var state = "NE";
+var zipCode = "68114";
+
+// output 
+console.log(firstName); 
+console.log(lastName); 
+console.log(age); 
+console.log(street); 
+console.log(city); 
+console.log(state); 
+console.log(zipCode); 
+
+// end program 
