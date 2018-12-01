bac79f29afe893a709c21ab48795a1583bcd1b6b
Andrew Hemminger <arhemminger@gmail.com>
2018-12-01 17:00:12 -0600

    assignment 1.3 commit

---
+++ week-1/Hemminger-Exercise1.3-Hello-World.js
@@ -0,0 +1,10 @@
+/* 
+============================================ ;
+Title: Assignment 1.3 ; 
+Author: Andrew Hemminger ; 
+Date: 1 December 2018 ; 
+Modified By: Andrew Hemminger ; 
+Description: This is the deliverable for; 
+exercise 1.3.;
+=========================================== 
+*/
