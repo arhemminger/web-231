387dfd21763a9dc3a3675d85e2c1d7d59c0a46d2
Andrew Hemminger <arhemminger@gmail.com>
2018-12-09 14:53:59 -0600

    Adding assignment 2.2-header

---
+++ week-2/hemminger-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  hemminger-header-test.js
+; Author: Professor Krasso 
+; Date:   9 December 2018
+; Modified By: Andrew Hemminger
+; Description: Tests the display of a formatted header
+;===========================================
+*/
+
+var header = require('./hemminger-header.js');
+
+console.log(header.display("Andrew", "Hemminger", "Assignment 2.2 - Header")); 
