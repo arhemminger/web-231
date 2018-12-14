d2d93142a64352a6e447ad21534bb5b222296ba0
Andrew Hemminger <arhemminger@gmail.com>
2018-12-13 22:01:48 -0600

    adding assignment 3.1

---
+++ week-3/hemminger-assignment-3.1.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title:  Control Statements
+; Author: Professor Krasso
+; Date:   13 December 2018
+; Modified by: Andrew Hemminger
+; Description: Sample program with errors.
+;===========================================
+*/
+var header = require('../hemminger-header.js');
+console.log(header.display("Andrew", "Hemminger", "Exercise 3.1"));
+console.log('\n');
+
+// Start program
+
+var x = 0,
+    y = 10;
+
+
+while (x < y || x < x - y) {
+  console.log("x = " + x);
+  X++;
+  console.log(y - x  " more iterations to go\n");
+}
+
+var z = "COMPLETE!";
+console.log(z);
+
+// End program
