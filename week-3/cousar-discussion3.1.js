8af3d8c8a12e781b8872f65a2c74022b9bf7bd84
Andrew Hemminger <arhemminger@gmail.com>
2018-12-16 15:48:02 -0600

    discussion board replys

---
+++ week-3/cousar-discussion3.1.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title:  cousar-db-3.1.js
+; Author: Donald Cousar
+; Date:   12/12/2018
+; Modified By: Andrew Hemminger
+; Description: Displays name in a sentence
+;===========================================
+
+Expected output:
+your name is: Don
+*/
+
+
+//Start Program
+
+//Pass name to function to get a string back describing input
+function showNamed(name) {
+    if (name.length > 0) {
+        return "your name is: " + name;
+      }
+     else {
+        return "check input:  Invalid name length";
+    }
+}
+
+//Display function output
+console.log(showNamed("Don"));
