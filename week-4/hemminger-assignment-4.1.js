21d07bcf4a53169a3733315c28af2d56ee78052b
Andrew Hemminger <arhemminger@gmail.com>
2019-01-06 18:11:26 -0600

    adding assignment 4.1 & 4.2

---
+++ week-4/hemminger-assignment-4.1.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title:  Arrays
+; Author: Professor Krasso
+; Date:   6 January 2019
+; Modified by: Andrew Hemminger
+; Description: Sample program with errors.
+;===========================================
+*/
+var header = require('../hemminger-header.js');
+console.log(header.display("Andrew", "Hemminger", "Exercise 4.1"));
+console.log('\n');
+
+// Start program
+
+var sample = new Array[ null, "Hi", 3.14, 0, false, 5,];
+remove a[1];
+sample.length = 6;
+
+//End program
---
+++ week-4/hemminger-assignment-4.2.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title:  Arrays
+; Author: Professor Krasso
+; Date:   6 January 2019
+; Modified by: Andrew Hemminger
+; Description: Sample program with errors.
+;===========================================
+*/
+var header = require('../hemminger-header.js');
+console.log(header.display("Andrew", "Hemminger", "Exercise 4.2"));
+console.log('\n');
+
+// Start program
+
+var a = ["Apple", "Kiwi", "Banana", "Orange", "Peach"];
+
+//function
+function getFruit(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+}
+
+//Output of getFruit() function
+getFruit(a);
